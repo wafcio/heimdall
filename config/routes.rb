b71cdbf952af07fe5a52b1f10f8efc3ea7b0cdf3
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2017-01-03 20:24:57 +0100

    remove postgresql streamer

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   resources :points
   resources :images, only: [:create, :destroy]
 
-  mount PostgresqlLoStreamer::Engine => "/category_image"
-
   match "/auth/:provider" => "omniauth_callbacks#passthru", via: [:get, :post], as: :omniauth
   match "/auth/:provider/callback" => "omniauth_callbacks#callback", via: [:get, :post]
   get "sign_out" => "omniauth_callbacks#sign_out", as: :sign_out
